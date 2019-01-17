6942820c61271ba7e24b19786ad7e4831cb685d0
Sagar Lama <sagarlama826@gmail.com>
2019-01-17 19:31:34 +0545

    Setup

---
+++ dev-tmux.sh
@@ -0,0 +1,8 @@
+tmux new-session \; \
+  split-window -v -p 20 \; \
+  split-window -h -p 30 \; \
+  send-keys 'top' C-m \; \
+  select-pane -t 1 \; \
+  send-keys 'conda activate myEnv' C-m\; \
+  select-pane -t 0 \; \
+  send-keys 'vim' C-m\;
